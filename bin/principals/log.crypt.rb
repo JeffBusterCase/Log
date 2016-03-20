e84525b4c247f6b35a44cda5b65563d65f960aa4
JeffBusterCase <jefferson_bomfim@outlook.com>
2016-03-19 23:07:45 -0300

    yet testing

--- bin/principals/log.crypt.rb
+++ bin/principals/log.crypt.rb
@@ -4,11 +4,12 @@ class Log
   private
   def crypt
     cipher = OpenSSL::Cipher.new('AES-128-CFB')
-    if @last_login[@primarKey].length >= 16
+    p @last_key
+    if @last_key.length >= 16
       # TODO: cryptograpth
       cipher.encrypt
-      cipher.key = @last_login[@primarKey]
-      @last_login[@primarKey] = cipher.update(@last_login[@primarKey]) + cipher.final
+      cipher.key = @last_key
+      @temp[@last_login][@primarKey] = cipher.update(@last_key) + cipher.final
     else
       raise "Key.length too short\nMinimum of '16' caracters"
     end
@@ -16,9 +17,9 @@ class Log
 
   def decrypter data
     decipher = OpenSSL::Cipher.new('AES-128-CFB')
-    if @last_log.length >= 16
+    if @last_key.length >= 16
       # TODO: descryptograpth
-      decipher.key = @last_log[@primarKey]
+      decipher.key = @last_key
       return (decipher.update(@temp[@last_login][@primarKey]) + decipher.final)
     else
       raise "Key.length too short\nMinimum of '16' caracters"
