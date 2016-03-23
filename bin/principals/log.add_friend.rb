eb5d8367bdcc4b9de13c4ec8e36c251e21cba9f3
Jefferson David Silva Bomfim <jefferson_bomfim@outlook.com>
2016-03-23 16:12:36 +0000

    iv_TAKE_CARE_OF_THIS IMPLEMENTATION

--- bin/principals/log.add_friend.rb
+++ bin/principals/log.add_friend.rb
@@ -19,16 +19,27 @@ class Log
     def accept_add_request login_and_password, of_who
         #Accept add request from a user mail in your user_data
         
+        #That care of this.
+        cipher = OpenSSL::Cipher('AES-128-CFB')
+        #Be carefull otherwise users will be hacked and somebody will
+        #be able to view their mesages
+        key = cipher.random_key
+        
+        #Make the iv be better than 16 caracteres
+        tmp_iv = @last_login.to_s + of_who.to_s
+        while tmp_iv.length < 16
+            tmp_iv << "_"
+        end
+        
+        iV_TAKE_CARE_OF_THIS = @tmp_iv
+        
         #if you want you can override and make a better from your ideas
         #But be careful here is this part
         #the login and password is really important***
         @last_login = login_and_password[@meta]
         @last_key = login_and_password[@primarKey]
         raise RuntimeError, "Invalid #{@meta} or #{@primarKey}" if !(@users.include? @last_login) &&  !(login)
-        #That care of this.
-        #Be carefull otherwise users will be hacked and somebody will
-        #be able to view their mesages
-        key
+        
         
         @userData[@last_login][:friends] << {
             @meta.to_sym => of_who,
